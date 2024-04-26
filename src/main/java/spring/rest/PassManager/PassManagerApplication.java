e1e64a266dbb28b6eccf37d7873173f20144cfe7
without eyes <37347347+without-eyes@users.noreply.github.com>
2024-04-26 22:15:10 +0300

    Initial commit

---
+++ src/main/java/spring/rest/PassManager/PassManagerApplication.java
@@ -0,0 +1,13 @@
+package spring.rest.PassManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassManagerApplication.class, args);
+	}
+
+}
